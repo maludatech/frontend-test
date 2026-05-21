d20875e81e0c5d5ef7a1fb79ad7cd3861cad8737
maludatech <nwitevictor7@gmail.com>
2026-05-21 13:26:25 +0100

    update: CTASection started

--- components/CTASection.tsx
+++ components/CTASection.tsx
@@ -1,3 +1,17 @@
 export const CTASection = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col sm:flex-row gap-4 justify-between">
+      <div>
+        <h1>Spots fill fast. Next term starts soon.</h1>
+        <h2>
+          Book at least a week before the term starts to secure your
+          child&apos;s place.
+        </h2>
+      </div>
+      <div className="flex gap-2">
+        <button>Show me the schedule</button>
+        <button>&gt;</button>
+      </div>
+    </div>
+  );
 };
